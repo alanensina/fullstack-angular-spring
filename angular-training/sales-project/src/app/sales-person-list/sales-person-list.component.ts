9535de9d31e2296901c74b144b69818d2842355f
Alan <alanvinicius.ce@gmail.com>
2021-06-06 14:36:31 -0300

    Added some angular features

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,20 +3,20 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
 
-// create an array of objects
-salesPersonList: SalesPerson[] = [
-  new SalesPerson('LeBron', 'James', 'lebron@lakers.com', 53000),
-  new SalesPerson('Kobe', 'Bryant', 'kobe@lakers.com', 45700),
-  new SalesPerson('Magic', 'Johnson', 'magic@lakers.com', 40200),
-  new SalesPerson('Shaquile', `O'Neal`, 'shaq@lakers.com', 33900),
-  new SalesPerson('Anthony', 'Davis', 'ad@lakers.com', 22780),
-  new SalesPerson('Alex', 'Caruso', 'caruso@lakers.com', 2340)
-];
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('John', 'James', 'john@esncorp.com', 53000),
+    new SalesPerson('Marie', 'Simpson', 'marie@esncorp.com', 45700),
+    new SalesPerson('Phill', 'Johnson', 'phil@esncorp.com', 12560),
+    new SalesPerson('Karl', `O'Neal`, 'karl@esncorp.com', 33900),
+    new SalesPerson('Anthony', 'Scottie', 'anthony@esncorp.com', 17569),
+    new SalesPerson('Jully', 'Tenezzi', 'jully@esncorp.com', 2340)
+  ];
 
   constructor() { }
 
