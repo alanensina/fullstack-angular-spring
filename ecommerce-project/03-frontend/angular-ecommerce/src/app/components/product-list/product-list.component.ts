354309d77b27b865684dbe06d57b22d82e269848
Alan Ensina <alanvinicius.ce@gmail.com>
2021-07-16 13:58:54 -0300

    Finished search of products by category

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,30 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
-
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
+ 
 @Component({
   selector: 'app-product-list',
-  //templateUrl: './product-list.component.html',
-  //templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+ 
   products!: Product[];
-
-  constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.listProducts();
+  currentCategoryId!: number;
+ 
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+ 
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
-
-  listProducts(){
-    this.productService.getProductList().subscribe(
+ 
+  listProducts() {
+    //check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+ 
+    if (hasCategoryId) {
+      //get the "id" param and convert it into a number
+      this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+    } else {
+      this.currentCategoryId = 1;
+    }
+ 
+    //get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
-    )
+    );
   }
 
 }
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,28 +1,30 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
+ 
   private baseUrl = 'http://localhost:8080/api/products';
-
+ 
   constructor(private httpClient: HttpClient) { }
-
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+ 
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    console.log(searchUrl)
+ 
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 }
-
-interface GetResponse{
-    _embedded: {
-      products: Product[];
-    }
+ 
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
-
